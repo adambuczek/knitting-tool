d77848801dd9ee38ac24b114ad70dfe8f19f6a09
Adam Buczek <adambuczek@gmail.com>
2021-12-16 09:09:38 +0100

    counter compnent

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import './App.css';
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 import CounterComponent from './Counter';
 
 function App() {
-
-
   return (
-    <div className="App">
-      <section>
-        <Button
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Edit Global Counter
-        </Button>
-        <CounterComponent></CounterComponent>
-      </section>
-    </div>
+    <Box sx={{ textAlign: 'center', mt: 5 }}>
+      <CounterComponent name="global" max={10} />
+    </Box>
   );
 }
 
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,73 +2,45 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddLinkIcon from '@mui/icons-material/AddLink';
 
-export class Counter {
-  name: string;
-  max: number;
-  id: string;
+function CounterComponent({ name, max = 10 }: { name: string, max: number }) {
+  const [counter, setCounter] = React.useState<number>(1);
 
-  value = 1;
-  children: Record<string, Counter> = {};
-
-  constructor(name: string, max: number) {
-    this.name = name;
-    this.max = max;
-    this.id = `${name}-${Math.random()}`;
-  }
-
-  addChild(child: Counter) {
-    this.children[child.id] = child;
-  }
-
-  removeChild(child: Counter) {
-    delete this.children[child.id];
-  }
-
-  increment() {
-    let newValue = this.value + 1;
-
-    if (this.max) {
-      newValue = (this.value >= this.max) ? 1 : newValue;
-    }
-    this.value = newValue;
-
-    Object.values(this.children).forEach(child => child.increment());
-  }
-
-  decrement() {
-    let newValue = this.value - 1;
-
-    if (this.max) {
-      newValue = (this.value <= 1) ? this.max : newValue;
-    }
-    this.value = newValue;
-
-    Object.values(this.children).forEach(child => child.decrement());
-  }
-}
-
-function CounterComponent() {
-  const [counter, setCounter] = React.useState<Counter>(new Counter('Counter', 10));
+  const id = `${name}-${Date.now()}`;
 
   const increment = () => {
-    counter.increment();
-    setCounter(counter);
+    let newValue = counter + 1;
+    if (max) {
+      newValue = (counter >= max) ? 1 : newValue;
+    }
+    setCounter(newValue);
   };
 
   const decrement = () => {
-    counter.decrement();
-    setCounter(counter);
+    let newValue = counter - 1;
+    if (max) {
+      newValue = (counter <= 1) ? max : newValue;
+    }
+    setCounter(newValue);
   };
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
+    <>
+    <Box sx={{ display: 'flex', justifyContent: 'space-around' }} data-counter-id={id}>
       <Button onClick={decrement}>-</Button>
       <Typography variant="h1" component="div">
-        {counter.value}
+        {counter}
       </Typography>
       <Button onClick={increment}>+</Button>
     </Box>
+    <Box sx={{ display: 'flex', justifyContent: 'space-around' }} data-counter-id={id}>
+      <Fab color="primary" aria-label="add" size="small">
+        <AddLinkIcon />
+      </Fab>
+    </Box>
+    </>
   );
 }
 
