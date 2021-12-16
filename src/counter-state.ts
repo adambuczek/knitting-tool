a7369538a74e3e0b99fa2ad3aa050ca03db9fed4
Adam Buczek <adambuczek@gmail.com>
2021-12-16 22:44:59 +0100

    store state locally

--- src/counter-state.ts
+++ src/counter-state.ts
@@ -23,7 +23,15 @@ export interface CountersStatePayload extends Partial<Counter> {};
 
 export const GLOBAL_COUNTER_ID = 'global';
 
-const countersState: CountersState = {
+let storedState = null;
+try {
+  const stateString = localStorage.getItem('counters');
+  storedState = stateString && JSON.parse(stateString);
+} catch (error) {
+  console.error(error);
+}
+
+const countersState: CountersState = storedState || {
   [GLOBAL_COUNTER_ID]: {
     id: GLOBAL_COUNTER_ID,
     value: 1
@@ -59,58 +67,68 @@ function countersReducer(
     action.payload = {};
   }
 
+  let newState: CountersState = {};
+
   switch (action.type) {
     case CountersStateActions.INCREMENT_GLOBAL:
-      return {
+      newState = {
         ...state,
         ...Object.entries(state).reduce((acc, [key, counter]) => {
           acc[key] = increment(counter);
           return acc;
         }, {} as typeof state)
       };
+      break;
 
     case CountersStateActions.DECREMENT_GLOBAL:
-      return {
+      newState = {
         ...state,
         ...Object.entries(state).reduce((acc, [key, counter]) => {
           acc[key] = decrement(counter);
           return acc;
         }, {} as typeof state)
       };
+      break;
 
     case CountersStateActions.INCREMENT:
       if (action.payload?.id) {
-        return {
+        newState = {
           ...state,
           [action.payload.id]: increment(state[action.payload.id])
         };
+      } else {
+        newState = state;
       }
-      return state;
+      break;
 
     case CountersStateActions.DECREMENT:
       if (action.payload?.id) {
-        return {
+        newState = {
           ...state,
           [action.payload.id]: decrement(state[action.payload.id])
         };
+      } else {
+        newState = state;
       }
-      return state;
+      break;
 
     case CountersStateActions.SET_MAX:
       if (action.payload?.id && action.payload?.max && state[action.payload.id]) {
-        return {
+        newState = {
           ...state,
           [action.payload.id]: {
             ...state[action.payload.id],
             max: action.payload.max
           }
         };
-      };
-      return state;
+      } else {
+        newState = state;
+      }
+      break;
       
     case CountersStateActions.ADD_COUNTER:
       const id = `${action.payload.name}-${Date.now()}`;
-      return {
+      newState = {
         ...state,
         [id]: {
           id,
@@ -119,22 +137,29 @@ function countersReducer(
           max: action.payload.max
         }
       };
+      break;
 
     case CountersStateActions.EDIT_COUNTER:
       if (action.payload?.id && state[action.payload.id]) {
-        return {
+        newState = {
           ...state,
           [action.payload.id]: {
             ...state[action.payload.id],
             ...action.payload
           }
         };
+      } else {
+        newState = state;
       }
-      return state;
+      break;
 
     default:
       throw new Error("Wrong action type");
   }
+
+  localStorage.setItem('counters', JSON.stringify(newState));
+
+  return newState;
 };
 
 export {
